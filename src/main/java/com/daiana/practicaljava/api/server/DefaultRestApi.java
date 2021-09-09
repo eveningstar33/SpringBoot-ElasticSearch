410dc89fceff4434534214905e84cd2e6128afd1
Daiana <admin@topmusicmakers.com>
2021-09-09 12:45:37 +0300

    Added loggging into the api

--- src/main/java/com/daiana/practicaljava/api/server/DefaultRestApi.java
+++ src/main/java/com/daiana/practicaljava/api/server/DefaultRestApi.java
@@ -1,20 +1,26 @@
 package com.daiana.practicaljava.api.server;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
+
 import java.time.LocalTime;
 
 @RestController
 @RequestMapping(value = "/api")
 public class DefaultRestApi {
 
+    private Logger LOG = LoggerFactory.getLogger(DefaultRestApi.class);
+
     @GetMapping(value = "/welcome", produces = MediaType.APPLICATION_JSON_VALUE)
     public String welcome() {
+        LOG.info(String.join("Hello, ", "this is ", "Spring Boot " , "API!"));
         return "Welcome to Spring Boot Updated!";
     }
 
